8abcdd19794f42c52e38f7e6baabe493242bf710
Samet Yorukoglu <samet.yorukoglu@trendyol.com>
2023-05-29 19:12:38 +0300

    fix

--- src/NumberReader/NumberReader.tsx
+++ src/NumberReader/NumberReader.tsx
@@ -60,7 +60,7 @@ const NumberReader = () => {
       if (arr[i] > first) {
         second = first;
         first = arr[i];
-      } else if (arr[i] > second && arr[i] != first) {
+      } else if (arr[i] > second && arr[i] !== first) {
         second = arr[i];
       }
     }
