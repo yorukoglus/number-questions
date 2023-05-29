c99ab57dcb52afbd711169b3db9da7e882dd38e9
Samet Yorukoglu <samet.yorukoglu@trendyol.com>
2023-05-29 19:11:19 +0300

    Add smallest & largest

--- src/NumberReader/NumberReader.tsx
+++ src/NumberReader/NumberReader.tsx
@@ -2,6 +2,7 @@ import "./NumberReader.scss";
 
 import { useEffect, useState } from "react";
 import Speech from "react-speech";
+import { convertNumberToWords } from "../utils/numberToWord";
 
 function randomGenerator(min: number, max: number, multiple: number) {
   return Math.round((Math.random() * (max - min) + min) / multiple) * multiple;
@@ -16,9 +17,58 @@ const NumberReader = () => {
   const [showResult, setShowResult] = useState<boolean>(false);
   const [showText, setShowText] = useState<boolean>(false);
   const [difficulty, setDifficulty] = useState<number>(0);
+  const [game, setGame] = useState<number>(-1);
+  const [order, setOrder] = useState<number>(1);
+
+  function speakMessage(message: string, PAUSE_MS = 500, splitter?: string) {
+    try {
+      const messageParts = message.split(splitter || ",");
+
+      let currentIndex = 0;
+      const speak = (textToSpeak: string) => {
+        const msg = new SpeechSynthesisUtterance();
+        const voices = window.speechSynthesis.getVoices();
+        msg.voice =
+          voices.find(({ name }) => name === "Google UK English Female") ||
+          voices[0];
+        msg.volume = 1; // 0 to 1
+        msg.rate = 1; // 0.1 to 10
+        msg.pitch = 1; // 0 to 2
+        msg.text = textToSpeak;
+        msg.lang = "en-US";
+
+        msg.onend = function () {
+          currentIndex++;
+          if (currentIndex < messageParts.length) {
+            setTimeout(() => {
+              speak(messageParts[currentIndex]);
+            }, PAUSE_MS);
+          }
+        };
+        speechSynthesis.speak(msg);
+      };
+      speak(messageParts[0]);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  function findSecondLargestElem(arr: number[]) {
+    let first = -1,
+      second = -1;
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+      if (arr[i] > first) {
+        second = first;
+        first = arr[i];
+      } else if (arr[i] > second && arr[i] != first) {
+        second = arr[i];
+      }
+    }
+    return second;
+  }
 
   const textCreator = () => {
-    let n = Math.floor(Math.random() * 2) + 1;
+    let n = game === -1 ? Math.floor(Math.random() * 3) + 1 : game;
 
     let t = "";
     if (n === 1) {
@@ -55,6 +105,30 @@ const NumberReader = () => {
 
       setResult(firstNum - secondNum);
       t = firstNum.toString() + " minus " + secondNum.toString();
+    } else if (n === 3 || n === 4) {
+      const numbers = [];
+      if (difficulty === 0) {
+        for (let i = 0; i < 4; i++) {
+          numbers.push(randomGenerator(10, 100, 5));
+        }
+      } else if (difficulty === 1) {
+        for (let i = 0; i < 5; i++) {
+          numbers.push(randomGenerator(10, 100, 1));
+        }
+      } else {
+        for (let i = 0; i < 7; i++) {
+          numbers.push(randomGenerator(10, 100, 1));
+        }
+      }
+      setResult(
+        order === 1 ? Math.max(...numbers) : findSecondLargestElem(numbers)
+      );
+      const predicator = n === 3 ? "largest" : "smallest";
+      t =
+        "Which one is the " +
+        (order === 1 ? predicator : "second " + predicator) +
+        " number? " +
+        numbers.map(convertNumberToWords).join("  ");
     }
     setText(t);
   };
@@ -77,7 +151,7 @@ const NumberReader = () => {
 
   useEffect(() => {
     textCreator();
-  }, []); //eslint-disable-line
+  }, [difficulty, game, order]); //eslint-disable-line
 
   return (
     <div className="number-reader">
@@ -103,26 +177,60 @@ const NumberReader = () => {
           <select
             name="difficulty"
             value={difficulty}
-            onChange={(e: any) => setDifficulty(e.target.value)}>
+            onChange={(e: any) => setDifficulty(Number(e.target.value))}>
             <option value={0}>Easy</option>
             <option value={1}>Medium</option>
             <option value={2}>Hard</option>
           </select>
+        </div>
+        <div className="config-element">
+          <label>Select Game:</label>
+          <select
+            name="game"
+            value={game}
+            onChange={(e: any) => setGame(Number(e.target.value))}>
+            <option value={-1}>Random</option>
+            <option value={1}>5 digit</option>
+            <option value={2}>Substraction</option>
+            <option value={3}>Largest</option>
+            <option value={4}>Smallest</option>
+          </select>
+
+          {[3, 4].includes(game) && (
+            <span style={{ marginLeft: "10px" }}>
+              <label>Order:</label>
+              <select
+                name="order"
+                value={order}
+                onChange={(e: any) => setOrder(Number(e.target.value))}>
+                <option value={1}>First</option>
+                <option value={2}>Second</option>
+              </select>
+            </span>
+          )}
         </div>
       </div>
       <div className="side right-side">
         <button className="restart-button" onClick={textCreator}>
           Restart
         </button>
-        <Speech
-          voice="Google UK English Female"
-          displayText={text}
-          text={text}
-          stop
-          pause
-          resume>
-          konus
-        </Speech>
+        {[3, 4].includes(game) ? (
+          <button
+            style={{ height: "40px" }}
+            onClick={() => speakMessage(text, 1000, "  ")}>
+            Start
+          </button>
+        ) : (
+          <Speech
+            voice="Google UK English Female"
+            displayText={text}
+            text={text}
+            stop
+            pause
+            resume>
+            konus
+          </Speech>
+        )}
         <div className="speech-text">{showText ? text : null}</div>
         <form onSubmit={handleSubmit} className="form">
           <label>Answer:</label>
